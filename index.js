24d49f8a0e87a4fb8175328731dda16abdd6bc2f
Jane Kamata <janekamata8@gmail.com>
2024-11-20 14:28:07 -0500

    Checking domain

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV !== "development") {
   sessionOptions.cookie = {
     sameSite: "none",
     secure: true,
-    domain: process.env.REMOTE_SERVER,
+    domain: process.env.NODE_SERVER_DOMAIN,
   };
 }
 app.use(session(sessionOptions));
