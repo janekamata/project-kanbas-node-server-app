2b5cb2822d0010e5586da76ad8e46debc848b2c5
Jane Kamata <janekamata8@gmail.com>
2024-11-29 20:32:29 -0500

    Completed 3

--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -3,7 +3,11 @@ import * as courseDao from "../Courses/dao.js";
 import * as enrollmentsDao from "../Enrollments/dao.js";
 
 export default function UserRoutes(app) {
-  const createUser = async (req, res) => {};
+  const createUser = async (req, res) => {
+    const user = await dao.createUser(req.body);
+    res.json(user);
+  };
+
   const deleteUser = async (req, res) => {
     const status = await dao.deleteUser(req.params.userId);
     res.json(status);
@@ -29,7 +33,7 @@ export default function UserRoutes(app) {
     res.json(user);
   };
   const updateUser = async (req, res) => {
-    const userId = req.params.userId;
+    const { userId } = req.params;
     const userUpdates = req.body;
     await dao.updateUser(userId, userUpdates);
     const currentUser = req.session["currentUser"];
