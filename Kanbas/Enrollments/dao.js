c4e5316a9eebc986c7985ce2a70cebb1a950a098
Jane Kamata <janekamata8@gmail.com>
2024-12-02 23:56:24 -0500

    Fixing map including null on populate

--- Kanbas/Enrollments/dao.js
+++ Kanbas/Enrollments/dao.js
@@ -5,11 +5,11 @@ export function findAllEnrollments() {
 }
 export async function findCoursesForUser(userId) {
   const enrollments = await model.find({ user: userId }).populate("course");
-  return enrollments.map((enrollment) => enrollment.course);
+  return enrollments.map((enrollment) => enrollment.course).filter(Boolean);
 }
 export async function findUsersForCourse(courseId) {
   const enrollments = await model.find({ course: courseId }).populate("user");
-  return enrollments.map((enrollment) => enrollment.user);
+  return enrollments.map((enrollment) => enrollment.user).filter(Boolean);
 }
 export function enrollUserInCourse(user, course) {
   return model.create({ user, course });
