41003a44a940a3766e9709d9348dee1870df519c
Ecoomber327 <78524305+Ecoomber327@users.noreply.github.com>
2024-12-07 13:42:46 -0500

    attempts added

---
+++ Kanbas/Attempts/schema.js
@@ -0,0 +1,38 @@
+// schema.js
+
+import mongoose from "mongoose";
+
+// Define a schema for choices within questions
+const choiceSchema = new mongoose.Schema({
+  correct: { type: Boolean, default: false },
+  answer: { type: String },
+});
+
+// Define a schema for quiz questions
+const questionSchema = new mongoose.Schema({
+  title: { type: String },
+  type: { type: String, default: "Multiple Choice" },
+  points: { type: Number, default: 0 },
+  question: { type: String, required: true },
+  choices: [choiceSchema],
+});
+
+// Define a schema for attempts
+const attemptSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    quiz: { type: mongoose.Schema.Types.ObjectId, ref: "Quiz", required: true },
+    questions: [
+      {
+        question: { type: mongoose.Schema.Types.ObjectId, ref: "Question" },
+        answer: { type: String },
+        correct: { type: Boolean },
+      },
+    ],
+    score: { type: Number, default: 0 },
+    lastAttempt: { type: Date, default: Date.now },
+  },
+  { collection: "attempts" }
+);
+
+export const AttemptsModel = mongoose.model("AttemptsModel", attemptSchema);
